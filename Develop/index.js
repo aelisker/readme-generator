fef3b26720a455a6d95bce80fe602cc03b7bdca2
Aaron Lisker <aelisker@gmail.com>
2020-05-06 19:47:15 -0700

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,36 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your name? (Required)'
+  }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = data => {
+  fs.writeFile('./README.md', data, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    else {
+      console.log('Your README.md file has been successfully created. Check it out!');
+    }
+  })
 }
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+inquirer.prompt(questions)
+.then(answers => {
+  return generateMarkdown(answers);
+})
+.then(readmeContent => {
+  return writeToFile(readmeContent);
+})
+.catch(err => {
+  console.log(err);
+});
